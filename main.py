db3c2cd9ca395267a6a863cafc52fcce54975bdb
SotaNorsu <pelaaja538@gmail.com>
2024-08-09 15:23:09 +0300

    Code Reformatted

--- main.py
+++ main.py
@@ -9,29 +9,23 @@ import secrets
 from tkinter import messagebox
 
 
-
 def main():
     root = tk.Tk()
     root.title("Disk Space Filler")
     #root.geometry("400x200")
-    root.resizable(0, 0)
+    root.resizable(False, False)
 
     selected = tk.StringVar()
     filetype_select = tk.StringVar()
 
-    
-    tk.Label(root, text="Disk Space Filler", font=("Helvetica", 20)).pack()
+    ttk.Label(root, text="Disk Space Filler", font=("Helvetica", 20)).pack()
 
-
-    tk.Label(root, text="Megabytes:").pack()
-
+    ttk.Label(root, text="Megabytes:").pack()
 
     entry = tk.Entry(root)
     entry.pack()
 
-   
-
-    tk.Label(root, text="Mode:").pack()
+    ttk.Label(root, text="Mode:").pack()
 
     r1 = ttk.Radiobutton(root, text="Secure Mode", variable=selected, value="secure")
     r1.pack()
@@ -39,15 +33,13 @@ def main():
     r2 = ttk.Radiobutton(root, text="Normal Mode", variable=selected, value="normal")
     r2.pack()
 
-
-    tk.Label(root, text="Filetype:").pack()
-
+    ttk.Label(root, text="Filetype:").pack()
 
     r3 = ttk.Radiobutton(root, text="txt", variable=filetype_select, value="txt")
     r3.pack()
 
     r4 = ttk.Radiobutton(root, text="py", variable=filetype_select, value="py")
-    r4.pack()  
+    r4.pack()
 
     r5 = ttk.Radiobutton(root, text="pdf", variable=filetype_select, value="pdf")
     r5.pack()
@@ -57,40 +49,34 @@ def main():
 
     r7 = ttk.Radiobutton(root, text="png", variable=filetype_select, value="png")
     r7.pack()
-    
 
-
-    
-    tk.Button(root, text="Fill", command=lambda: fill_disk_space(entry.get(), selected.get(), root, filetype_select.get())).pack()
-
+    tk.Button(root, text="Fill",
+              command=lambda: fill_disk_space(entry.get(), selected.get(), root, filetype_select.get())).pack()
 
     root.mainloop()
+
 
 def fill_disk_space(megabytes, is_secure, master, filetype):
     try:
         megabytes = int(megabytes)
-        
+
     except ValueError:
         messagebox.showerror("Error", "Invalid input")
         return
-        
-        
-
 
     indicator = tk.Toplevel(master)
     indicator.title("Please wait...")
     indicator.resizable(0, 0)
 
     progress_var = tk.DoubleVar()
-    progress_bar = ttk.Progressbar(indicator, orient="horizontal", mode="determinate", maximum=megabytes, variable=progress_var)
+    progress_bar = ttk.Progressbar(indicator, orient="horizontal", mode="determinate", maximum=megabytes,
+                                   variable=progress_var)
     progress_bar.pack(pady=20)
-
 
     try:
         filetype = str(filetype)
     except ValueError:
         filetype = "txt"
-    
 
     for i in range(0, megabytes):
         file = open(f"{i * random.randint(0, 245)}.{filetype}", "wb")
@@ -109,6 +95,7 @@ def fill_disk_space(megabytes, is_secure, master, filetype):
     indicator.destroy()
 
     messagebox.showinfo("", "Operation Complete")
+
 
 if __name__ == "__main__":
     main()
